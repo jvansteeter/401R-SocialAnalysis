1a13c82d84cf46fa24d9d680ee0b799f8be08939
jvansteeter <spam.magnet1991@gmail.com>
2017-04-14 11:50:48 -0600

    stff

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
 import edu.stanford.nlp.simple.Sentence;
+import weka.classifiers.functions.Dl4jMlpClassifier;
 import weka.classifiers.functions.MultilayerPerceptron;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.CSVLoader;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.ObjectInputStream;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        SentimentAnalyzer test = new SentimentAnalyzer();
-        test.findSentiment("I hate everything so freaking much!");
+//        SentimentAnalyzer test = new SentimentAnalyzer();
+//        test.findSentiment("I hate everything so freaking much!");
 //        MultilayerPerceptron perceptron = new MultilayerPerceptron();
 //        perceptron.run();
 //        ArffLoader loader = new ArffLoader();
@@ -30,6 +33,8 @@ public class Main
 
 //Instance of NN
             MultilayerPerceptron mlp = new MultilayerPerceptron();
+            Dl4jMlpClassifier deepNet = (Dl4jMlpClassifier) (new ObjectInputStream(new FileInputStream("lia.model"))).readObject();
+            deepNet.classifyInstance();
 
 //Setting Parameters
             mlp.setLearningRate(0.1);
@@ -41,6 +46,7 @@ public class Main
 
             loader.setFile(new File("test.csv"));
             Instance test2 = loader.getNextInstance(train);
+
 
             System.out.println(mlp.classifyInstance(test2));
         }
